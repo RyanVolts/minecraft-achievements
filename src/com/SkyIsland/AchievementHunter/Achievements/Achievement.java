59c41a915b6d42d123f8e35c89745277b231a802
Skyler Manzanares <skymanzanares@hotmail.com>
2015-11-06 19:19:08 -0700

    Began skeleton for achievements

---
+++ src/com/SkyIsland/AchievementHunter/Achievements/Achievement.java
@@ -0,0 +1,21 @@
+package com.SkyIsland.AchievementHunter.Achievements;
+
+import org.bukkit.event.Listener;
+
+/**
+ * An achievement handles figuring out if players have unlocked themselves.<br />
+ * They are responsible for updating the player records kept by the player manager and for actually
+ * doing the listening and figuring out if the achievement requirements have been met.
+ */
+public abstract class Achievement implements Listener {
+
+	private String name;
+	
+	protected Achievement(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
